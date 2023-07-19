cf1de5882475e4ef4ad89218a11ff4a358fc2c10
haymanjoyce <haymanjoyce@gmail.com>
2023-07-19 16:04:02 +0200

    Renamed main module

--- src/tool/main.py
+++ src/tool/main.py
@@ -1,25 +1,1 @@
-#main.py
-
-#!/usr/bin/python3
-
-# import commands
-#
-# if __name__ == '__main__':
-#     commands.main()
-
-# python main.py test arg
-
-# /Users/richard/PycharmProjects/mt_1.0/src/tool/main.py
-
-
-
-import click
-
-
-@click.command()
-def main():
-    print("I'm a beautiful CLI ✨")
-
-if __name__ == "__main__":
-    main()
-
+#!/usr/bin/env python3# import commands# if __name__ == '__main__':#     commands.main()# python main.py test arg# /Users/richard/PycharmProjects/mt_1.0/src/tool/main.pyimport click@click.command()def main():    print("I'm a beautiful CLI ✨")if __name__ == "__main__":    main()
