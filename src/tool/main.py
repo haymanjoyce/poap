f828b923cec02af2fe369e3f127668038b6a8528
haymanjoyce <haymanjoyce@gmail.com>
2023-08-04 11:19:48 +0100

    Dropped main.py; entry point is tool.py in root directory.

--- src/tool/main.py
+++
@@ -1,1 +0,0 @@
-#!/usr/bin/env python3# import commands# if __name__ == '__main__':#     commands.main()# python main.py test arg# /Users/richard/PycharmProjects/mt_1.0/src/tool/main.pyimport click@click.command()def main():    print("I'm a beautiful CLI ✨")
--- tool.py
+++ tool.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 from src.tool.commands import main
 
 
 if __name__ == "__main__":
     main()
 
-
+# (mt_1.0) richard@Richards-MacBook-Pro-2 mt_1.0 % python -m tool test arg
