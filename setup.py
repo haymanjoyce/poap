8f135ce03fc9b4b0890e1f68fd613c08703742b1
haymanjoyce <haymanjoyce@gmail.com>
2023-10-23 14:38:57 +0100

    Found bug in install; commas missing from install_requires in setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'click==8.1.3'
-        'colorama==0.4.6'
-        'numpy==1.24.3'
-        'pandas==2.0.2'
-        'python-dateutil==2.8.2'
-        'pytz==2023.3'
-        'six==1.16.0'
-        'tzdata==2023.3'
+        'click==8.1.3',
+        'colorama==0.4.6',
+        'numpy==1.24.3',
+        'pandas==2.0.2',
+        'python-dateutil==2.8.2',
+        'pytz==2023.3',
+        'six==1.16.0',
+        'tzdata==2023.3',
     ],
     entry_points={
         'console_scripts': [
