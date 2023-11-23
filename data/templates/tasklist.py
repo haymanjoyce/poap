12ec3836f0dce8fdb6f8b6dc633769020f666681
haymanjoyce <haymanjoyce@gmail.com>
2023-11-23 18:47:20 +0000

    Discover py file type to be best format for storing df template data.

---
+++ data/templates/tasklist.py
@@ -0,0 +1,9 @@
+
+data = {
+    'Tasklist': ['Task A', 'Task B', 'Task C'],
+    'Start': ['date a', 'date b', 'date c'],
+    'Finish': ['date a', 'date b', 'date c'],
+}
+
+# import pandas types
+
