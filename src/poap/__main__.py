749ec6a64d104999f8f8b987df5c5c97c8fc97ee
haymanjoyce <haymanjoyce@gmail.com>
2024-02-03 19:34:37 +0000

    Add library.

--- src/poap/__main__.py
+++ src/poap/__main__.py
@@ -3,6 +3,7 @@ import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 
+
 class Window(QMainWindow):
     """Main Window."""
     def __init__(self, parent=None):
@@ -13,6 +14,7 @@ class Window(QMainWindow):
         self.centralWidget = QLabel("Hello, World")
         self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         self.setCentralWidget(self.centralWidget)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
