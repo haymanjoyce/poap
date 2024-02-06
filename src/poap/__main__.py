b833881d9a5a84ec3aaa65c589f5ac35dabb14cd
haymanjoyce <haymanjoyce@gmail.com>
2024-02-06 13:25:19 +0000

    Qt 5 and 6 do not support guassian blurring in SVG rendering.

--- src/poap/__main__.py
+++ src/poap/__main__.py
@@ -1,7 +1,8 @@
 import sys
 
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QRect
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtSvg import QSvgWidget
 
 
 class Window(QMainWindow):
@@ -10,10 +11,17 @@ class Window(QMainWindow):
         """Initializer."""
         super().__init__(parent)
         self.setWindowTitle("Python Menus & Toolbars")
-        self.resize(400, 200)
-        self.centralWidget = QLabel("Hello, World")
-        self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-        self.setCentralWidget(self.centralWidget)
+        # self.resize(400, 200)
+        # self.centralWidget = QLabel("Hello, World")
+        # self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        # self.setCentralWidget(self.centralWidget)
+        self.create_svg()
+
+    def create_svg(self):
+        svg = QSvgWidget('../../data/svgs/gaussian1.svg', parent=self)
+        svg.setGeometry(QRect(0, 0, 800, 600))
+        self.resize(svg.width(), svg.height())
+        svg.show()
 
 
 if __name__ == "__main__":
