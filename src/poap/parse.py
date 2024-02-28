00cc3672bf038044fb1ce3680af718b518efea2b
haymanjoyce <haymanjoyce@gmail.com>
2024-02-28 13:04:58 +0000

    Updated notes.

--- src/poap/parse.py
+++ src/poap/parse.py
@@ -2,15 +2,15 @@ import svgwrite
 import pandas as pd
 
 
-df3 = pd.read_excel('data/excel/sample.xlsx')
+df3 = pd.read_excel('../../data/excel/sample.xlsx')
 
 print(df3)
 
-dwg = svgwrite.Drawing('data/svg/test.svg', profile='tiny')
+dwg = svgwrite.Drawing('../../data/svg/test.svg', profile='tiny')
 
 for item in df3['a']:
     print(f"{item}")
-    dwg.add(dwg.rect(insert=(50, (item*50)), size=(100, 40), fill='red'))
+    dwg.add(dwg.rect(insert=(50, (item*50)), size=(100, 40), fill='green'))
 
 dwg.save()
 
