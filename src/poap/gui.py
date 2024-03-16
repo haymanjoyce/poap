96aad8c81bcc159acfdc953f6febdecd918c84d7
haymanjoyce <haymanjoyce@gmail.com>
2024-03-16 20:26:03 +0000

    Improved GUI

--- src/poap/gui.py
+++ src/poap/gui.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget
 from PyQt5.QtSvg import QSvgWidget
 from PyQt5.QtGui import QIcon
 
@@ -17,34 +17,80 @@ class MainWindow(QMainWindow):
 
         # Create a QSvgWidget
         svg_widget = QSvgWidget()
-
         # Load the SVG image
         svg_widget.load(SAMPLE_SVG_PATH)
 
-        # Set the central widget
-        self.setCentralWidget(svg_widget)
+        # Create a QPushButton
+        button = QPushButton("Refresh")
+        # Connect the clicked signal of the button to a function
+        button.clicked.connect(self.refresh)
+
+        # Create a QVBoxLayout
+        layout = QVBoxLayout()
+        # Add the svg_widget and the button to the layout
+        layout.addWidget(svg_widget)
+        layout.addWidget(button)
+
+        # Create a QWidget and set it as the central widget
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+        # Resize the window
+        self.resize(600, 600)
 
         # Create a menu bar
         menu_bar = self.menuBar()
 
         # Create a menu
-        file_menu = QMenu("File", self)
-        menu_bar.addMenu(file_menu)
+        data_menu = QMenu("Data", self)
+        download_menu = QMenu("Download", self)
+        help_menu = QMenu("Help", self)
+
+        # Add menus to the menu bar
+        menu_bar.addMenu(data_menu)
+        menu_bar.addMenu(download_menu)
+        menu_bar.addMenu(help_menu)
 
         # Create actions
-        open_action = QAction("Open", self)
-        download_template_action = QAction("Download Template", self)
-
-        # Connect the triggered signal of the open_action to the open_file function
-        open_action.triggered.connect(self.open_file)  # noinspection PyUnresolvedReferences
+        select_data_action = QAction("Select data", self)
+        check_data_action = QAction("Check data", self)
+        download_template_action = QAction("Download template", self)
+        download_svg_action = QAction("Download SVG file", self)
 
         # Add actions to the menu
-        file_menu.addAction(open_action)
-        file_menu.addAction(download_template_action)
+        data_menu.addAction(select_data_action)
+        data_menu.addAction(check_data_action)
+        download_menu.addAction(download_svg_action)
+        help_menu.addAction(download_template_action)
 
-        # Resize the window
-        self.resize(600, 600)
+        # Connect the triggered signal of the open_action to the open_file function
+        select_data_action.triggered.connect(self.select_data)
+        check_data_action.triggered.connect(self.check_data)
+        download_svg_action.triggered.connect(self.download_svg)
+        download_template_action.triggered.connect(self.download_template)
 
-    def open_file(self):
-        # This function will be executed when the "Open" action is triggered
-        print("Open action triggered")
+    def refresh(self):
+        # This function will be executed when the "Refresh" button is clicked
+        print("Refresh button clicked")
+
+    def select_data(self):
+        # This function will be executed when the "Select Data" action is triggered
+        print("Select Data action triggered")
+
+    def check_data(self):
+        # This function will be executed when the "Check Data" action is triggered
+        print("Check Data action triggered")
+
+    def download_template(self):
+        # This function will be executed when the "Download Template" action is triggered
+        print("Download Template action triggered")
+
+    def download_svg(self):
+        # This function will be executed when the "Download SVG" action is triggered
+        print("Download SVG action triggered")
+
+    def closeEvent(self, event):
+        # This function will be executed when the window is closed
+        print("Window closed")
+        event.accept()
