a6465435f9b4f1ad00b86c2f286ea1eebc05b8e0
haymanjoyce <haymanjoyce@gmail.com>
2024-04-07 09:48:29 +0100

    Updated template.

--- src/poap/template.py
+++ src/poap/template.py
@@ -1,4 +1,4 @@
-sheet_names = ['drawing', 'layout', 'scales', 'timeframes', 'tasks']
+sheet_names = ['drawing', 'layout', 'scales', 'timeframes', 'rows', 'tasks']
 
 drawing = {
     'drawing_width': 200,
@@ -8,19 +8,35 @@ drawing = {
 
 layout = {
     'header_height': 0.1,
-    'scale_height': 0.1,
-    'row_quantity': 5,
     'footer_height': 0.1,
+    'task_list_width': 0.2,
+    'top_scale_height': 0.1,
+    'bottom_scale_height': 0.1,
     'layout_colour': 'grey',
 }
 
-scale_units = ['days', 'weeks', 'months', 'years']
+scales = {
+    'units': ['days', 'weeks', 'months', 'years'],
+    'background_colour': 'white',
+    'font_colour': 'black',
+    'font_size': [8, 10, 12, 14],
+}
 
-timeframe_spans = [0.5, 0.25, 0.25]
-timeframe_start_dates = ['2024-01-01', '2024-07-01', '2025-03-01']
-timeframe_finish_dates = ['2024-03-01', '2024-09-01', '2025-06-01']
+timeframes = {
+    'timeframe': ['timeframe1', 'timeframe2', 'timeframe3'],
+    'timeframe_span': [0.5, 0.25, 0.25],
+    'start_date': ['2024-01-01', '2024-07-01', '2025-03-01'],
+    'finish_date': ['2024-03-01', '2024-09-01', '2025-06-01'],
+}
 
-tasks_columns = ['task', 'start', 'finish']
-tasks_tasks = ['task1', 'task2', 'task3']
-tasks_start_dates = ['2024-01-01', '2024-01-03', '2024-04-01']
-tasks_finish_dates = ['2024-01-30', '2024-08-01', '2025-03-01']
+rows = {
+    'row': ['row1', 'row2', 'row3'],
+    'row_height': [0.3, 0.3, 0.3],
+    'row_colour': ['white', 'grey', 'white'],
+}
+
+tasks = {
+    'task': ['task1', 'task2', 'task3'],
+    'start': ['2024-01-01', '2024-01-03', '2024-04-01'],
+    'finish': ['2024-01-30', '2024-08-01', '2025-03-01'],
+}
