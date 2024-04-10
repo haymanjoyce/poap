8d11f1c6d4a6cd54a193c5be329a6e4582f922ef
haymanjoyce <haymanjoyce@gmail.com>
2024-04-10 15:33:43 +0100

    Built Excel functions.

--- src/poap/excel.py
+++ src/poap/excel.py
@@ -26,15 +26,24 @@ def return_df_col_labels(data: Dict) -> pd.DataFrame:
     return df
 
 
-def return_excel_file(dfs: List, file_path: str = SAMPLE_XLSX_PATH) -> None:
-    pass
+def return_excel_file(dfs: List[pd.DataFrame], sheet_names: List[str], file_path: str = SAMPLE_XLSX_PATH) -> None:
+    with pd.ExcelWriter(file_path) as writer:
+        for df, sheet_name in zip(dfs, sheet_names):
+            df.to_excel(writer, sheet_name=sheet_name)
 
 
-def return_dfs(excel_file: Optional[str] = None) -> List:
-    pass
+def return_dfs(excel_file: Optional[str] = SAMPLE_XLSX_PATH) -> List[pd.DataFrame]:
+    xls = pd.ExcelFile(excel_file)
+    _sheet_names = xls.sheet_names
+    dfs = [xls.parse(sheet_name) for sheet_name in _sheet_names]
+    return dfs
 
+
+# todo ensure df variable name same as sheet name
 
 from src.poap.template import *
-
-return_df_col_labels(scales)
+# scales = return_df_col_labels(scales)
+# return_excel_file([scales,], '../../data/excel/sample.xlsx')
+# dfs = return_dfs('../../data/excel/sample.xlsx')
+# print(dfs)
 
