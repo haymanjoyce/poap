353ac8f2680c8af54e442f7c44c1f2d435606eb9
haymanjoyce <haymanjoyce@gmail.com>
2024-04-15 08:20:42 +0100

    Dropped scales from template and so dfs named when formed.

--- src/poap/excel.py
+++ src/poap/excel.py
@@ -10,40 +10,39 @@ import pandas as pd
 from src.poap.config import SAMPLE_XLSX_PATH
 
 
-def return_df_row_labels(data: Dict) -> pd.DataFrame:
+def return_df_row_labels(data: Dict, name: str) -> pd.DataFrame:
     keys = data.keys()
     values = data.values()
     df = pd.DataFrame(columns=['key', 'value'])
     df['key'] = list(keys)
     df['value'] = list(values)
-    print(df)
+    df.name = name
     return df
 
 
-def return_df_col_labels(data: Dict) -> pd.DataFrame:
+def return_df_col_labels(data: Dict, name: str) -> pd.DataFrame:
     df = pd.DataFrame(data)
-    print(df)
+    df.name = name
     return df
 
 
-def return_excel_file(dfs: List[pd.DataFrame], sheet_names: List[str], file_path: str = SAMPLE_XLSX_PATH) -> None:
+def save_excel_file(_dfs: List[pd.DataFrame], file_path: str = SAMPLE_XLSX_PATH) -> None:
     with pd.ExcelWriter(file_path) as writer:
-        for df, sheet_name in zip(dfs, sheet_names):
-            df.to_excel(writer, sheet_name=sheet_name)
+        for df in _dfs:
+            df.to_excel(writer, sheet_name=df.name)
 
 
 def return_dfs(excel_file: Optional[str] = SAMPLE_XLSX_PATH) -> List[pd.DataFrame]:
     xls = pd.ExcelFile(excel_file)
-    _sheet_names = xls.sheet_names
-    dfs = [xls.parse(sheet_name) for sheet_name in _sheet_names]
-    return dfs
+    _dfs = [xls.parse(sheet_name) for sheet_name in xls.sheet_names]
+    for i, _df in enumerate(_dfs):
+        _df.name = xls.sheet_names[i]
+    return _dfs
 
 
-# todo ensure df variable name same as sheet name
-
-from src.poap.template import *
-# scales = return_df_col_labels(scales)
-# return_excel_file([scales,], '../../data/excel/sample.xlsx')
+# from src.poap.template import *
+# scales = return_df_col_labels(scales, 'scales')
+# save_excel_file([scales, ], '../../data/excel/sample.xlsx')
 # dfs = return_dfs('../../data/excel/sample.xlsx')
-# print(dfs)
-
+# df = dfs[0]
+# print(df.name)
